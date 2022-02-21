2895e93ff7f2da53323aa0beeac3f1882adfaec8
alainlo1 <75679323+inestellagreen@users.noreply.github.com>
2022-02-21 13:59:02 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="blank"></div>
+    <div class="wrapper">
+      <div class="img1"></div>
+      <h2 class="title1">lorem ipsum <br><span>aaaaaa</span></h2>
+      <div class="img2"></div>
+      <h2 class="title2">lorem ipsum <br>  aaaaaa</h2>
+    </div>
+    <div class="blank"></div>
+  </div>
+</template>
+<script>
+
+import { gsap } from 'gsap'
+
+export default {
+  mounted: function (){
+    this.startAnimations()
+  },
+  methods: {
+    startAnimations: function(){
+      gsap.to('#title',{duartion: 1, color:'red'})
+    }
+  }
+
+}
+</script>
+
+
+<style>
+  *{
+    padding: 0;
+    margin: 0;
+  }
+  .blank{
+    height: 100vh;
+    width: 100%;
+  }
+
+</style>
